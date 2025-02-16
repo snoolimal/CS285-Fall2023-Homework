fab95bc138026905655db77ed7b401b6814c3799
Soonwoo Kwon <soonoolimal@gmail.com>
2025-02-17 01:01:45 +0900

    done

---
+++ hw1/cs285/scripts/params_hw1.py
@@ -0,0 +1,76 @@
+"""
+Set hyperparameters and parse.
+"""
+
+import argparse
+import time
+from pathlib import Path
+
+
+MAX_NVIDEO = 2      # tensorboard에 video로 저장할 rollouts의 수
+MAX_VIDEO_LEN = 40  # training loop에서 덮어 쓸 것
+
+MJ_ENV_NAMES = ['Ant-v4', 'Walker2d-v4', 'HalfCheetah-v4', 'Hopper-v4']
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # expert loading (이 script를 실행하는 dir에 따라 설정)
+    # parser.add_argument('--expert_policy_file', '-epf', type=str, required=True)
+    # parser.add_argument('--expert_data', '-ed', type=str, required=True)
+
+    # experiment
+    parser.add_argument('--exp_name', '-exp', type=str, default='pick an experiment name', required=True)
+    parser.add_argument('--env_name', '-env', type=str, help=f'choices: {", ".join(MJ_ENV_NAMES)}', required=True)
+    parser.add_argument('--do_dagger', action='store_true')
+
+    # policy network
+    parser.add_argument('--n_layers', type=int, default=2)
+    parser.add_argument('--hidden_size', type=int, default=64)
+
+    # rolling out
+    parser.add_argument('--ep_len', type=int)
+    parser.add_argument('--batch_size', type=int, default=1000)  # 각 iter에서 sampling할 transitions의 수
+    parser.add_argument('--max_replay_buffer_size', type=int, default=1000000)
+
+    # training
+    parser.add_argument('--n_iter', '-n', type=int, default=1)  # vanilla BC라면 1, DAgger라면 DAgger iteration 횟수
+    parser.add_argument('--num_agent_train_steps_per_iter', type=int, default=1000)  # 각 iter에서 밟을 grad steps의 수
+    parser.add_argument('--train_batch_size', type=int, default=100)  # 각 grad step에 사용하는 (training) trainsitions의 수
+    parser.add_argument('--lr', '-lr', type=float, default=5e-3)
+
+    # validation
+    parser.add_argument('--eval_batch_size',
+                        type=int, default=1000)  # eval metrics log를 찍을 validation data point의 수
+
+    # logging
+    parser.add_argument('--video_log_freq', type=int, default=5, help='-1 for not logging video')
+    parser.add_argument('--scalar_log_freq', type=int, default=1)
+    parser.add_argument('--save_params', action='store_true')
+
+    # seed and gpu
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--no_gpu', '-ngpu', action='store_true')
+    parser.add_argument('--which_gpu', type=int, default=0)
+
+    args = parser.parse_args()
+    params = vars(args)         # convert args to dict
+
+    # create directory for logging
+    if args.do_dagger:
+        logdir_prefix = 'q2_'
+        assert args.n_iter > 1, ("DAgger needs more than 1 iteration, i.e., n_iter>1, of training,"
+                                 "to iteratively query the expert and train (after 1st warmstarting from BC).")
+    else:
+        logdir_prefix = 'q1_'
+        assert args.n_iter == 1, "Vanilla behavior cloning collects expert data just once (n_iter=1)."
+    _cs285 = Path(__file__).resolve().parents[1]
+    data_path = _cs285 / 'data'
+    data_path.mkdir(exist_ok=True)
+    logdir = logdir_prefix + args.exp_name + '_' + args.env_name + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = data_path / logdir
+    params['logdir'] = str(logdir)
+    logdir.mkdir(exist_ok=True)
+
+    return params
--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -1,3 +1,178 @@
 """
 Runs behavior cloning and DAgger for homework 1
 """
+
+
+import pickle
+import numpy as np
+
+import torch
+import gym
+
+from cs285.policies.MLP_policy import MLPPolicySL
+from cs285.policies.loaded_gaussian_policy import LoadedGaussianPolicy
+from cs285.infrastructure.replay_buffer import ReplayBuffer
+from cs285.infrastructure import utils
+from cs285.infrastructure import pytorch_util as ptu
+from cs285.infrastructure.logger import Logger
+from cs285.scripts.params_hw1 import *
+
+
+def run_training_loop(params):
+    # set seed
+    seed = params['seed']
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    ptu.init_gpu(
+        use_gpu=not params['no_gpu'],
+        gpu_id=params['which_gpu']
+    )
+
+    # create logger
+    logger = Logger(params['logdir'])
+
+    ### ENV ###
+    # make the gym environment
+    env = gym.make(params['env_name'], render_mode=None)    # make and init the gym env
+    _, _ = env.reset(seed=seed)                             # reset the env to an init ob and returns the init ob, info
+
+    # max length for episodes
+    params['ep_len'] = params['ep_len'] or env.spec.max_episode_steps
+    MAX_VIDEO_LEN = params['ep_len']
+
+    # env attributes
+    assert isinstance(env.action_space, gym.spaces.Box), ("Action space must be continous,"
+                                                          "i.e., policy is contionous (conditoonal) distribution.")
+    ob_dim = env.observation_space.shape[0]     # condition random variable의 차원
+    ac_dim = env.action_space.shape[0]          # random variable의 차원
+
+    # simulation timestep (video 저장에 사용)
+    if hasattr(env, 'sim') and hasattr(env.sim, 'model'):   # simulation이 가능한 MuJoCo env라면
+        fps = 1 / env.sim.model.opt.timestep
+    else:
+        fps = env.metadata['render_fps']    # MJ_ENV_NAMES의 env들은 wrapper env가 아닌 base env이므로 env.env 불필요
+
+    ### AGENT (POLICY) ###
+    actor = MLPPolicySL(
+        ac_dim,
+        ob_dim,
+        params['n_layers'],
+        params['hidden_size'],
+        lr=params['lr']
+    )
+
+    # replay buffer
+    replay_buffer = ReplayBuffer(params['max_replay_buffer_size'])
+
+    ### EXPERT POLICY ###
+    _cs285 = Path(__file__).resolve().parents[1]
+    _expert_policy = params['env_name'].split('-')[0]
+    expert_policy_file = str(_cs285 / 'policies' / 'experts' / _expert_policy)
+
+    print('Loading expert policy from...', expert_policy_file)
+    expert_policy = LoadedGaussianPolicy(expert_policy_file)
+    expert_policy.to(ptu.device)
+    print('Done restoring expert policy.')
+
+    ### TRAINING LOOP ###
+    # init vars at beginning of training
+    total_envsteps = 0
+    start_time = time.time()
+
+    for itr in range(params['n_iter']):
+        print(f'\n\n********** Iteration %{itr} ************')
+
+        ## Data Collection (Rolling Out)
+        print('\nCollecting data to be used for training...')
+        # BC training from expert data
+        if itr == 0:
+            expert_data = str(_cs285 / 'expert_data' / f"expert_data_{params['env_name']}")
+            trajs = pickle.load(open(expert_data, 'rb'))
+            envsteps_this_batch = 0
+        # DAgger training from sampled data relabeled by expert
+        else:
+            assert params['do_dagger']
+            # sampling transitions
+            trajs, envsteps_this_batch = utils.sample_trajectories(
+                env, actor, params['batch_size'], params['ep_len']
+            )
+
+            print('\nRelabelling collected observations (from policy) with labels from expert policy...')
+            for i in range(len(trajs)):
+                trajs[i]['acs'] = expert_policy.get_action(trajs[i]['obs'])
+                # ---
+                # batch의 transition는 get_action()로 모은다.
+                # get_action()은 single observation을 받아 single action을 뱉어야 sequential한 sampling이 가능하다.
+                # 그러나 여기선 temporal context를 무시하고 batch를 한 번에 expert policy network에 forward passing시켜도 된다.
+                # 왜냐하면 DAgger에서 필요한 건,
+                # 실제로 expert policy를 env에 굴려 action을 sampling하는 것이 아니라 -- 애초에 expert policy를 env에 굴리는 순간
+                # actor의 observation을 공유할 수 없게 된다 --,
+                # actor가 모은 (batch) observation의 input에 mapping되는 action output을 얻는 것이기 때문이다.
+                # Actor가 모은 batch observation 이미지를 하나씩 보고 human demonstrator가 label을 하나씩 달아 줄 뿐,
+                # batch observation의 temporal context는 labeling에 고려하지 않는다.
+                # ---
+        total_envsteps += envsteps_this_batch
+        replay_buffer.add_rollouts(trajs)
+
+        ## Training Agent
+        print('\nTraining agent using sampled data from replay buffer...')
+        training_logs = []
+        for _ in range(params['num_agent_train_steps_per_iter']):
+            # sample transitions from replay buffer (imitation learning은 observation과 action만 필요)
+            indices = np.random.permutation(len(replay_buffer.observations))[:params['train_batch_size']]
+            ob_batch, ac_batch = replay_buffer.observations[indices], replay_buffer.actions[indices]
+
+            # take gradient step and log training performance
+            train_log = actor.update(ob_batch, ac_batch)
+            training_logs.append(train_log)
+
+        ## Evaluate and Log
+        print('\nBeginning logging procedure...')
+        log_video = ((params['video_log_freq'] != -1) and
+                     (itr % params['video_log_freq'] == 0))     # 요번 iter에서 video를 rendering하고 logging할 것인지
+        log_metrics = (itr % params['scalar_log_freq'] == 0)    # 요번 iter에서 eval metrics를 logging할 것인지
+
+        if log_video:
+            # save eval rollouts as videos in tensorboard event file
+            print('\nCollecting video of evaluation rollouts...')
+            eval_video_trajs = utils.sample_n_trajectories(
+                env, actor, MAX_NVIDEO, MAX_VIDEO_LEN, True
+            )
+            logger.log_trajs_as_videos(
+                trajs=eval_video_trajs,
+                step=itr,
+                max_videos_to_save=MAX_NVIDEO,
+                video_title='eval_rollouts'
+            )
+
+        if log_metrics:
+            # get eval metrics from (eval) transitions
+            print('\nCollecting eval rollouts...')
+            eval_trajs, eval_envsteps_this_batch = utils.sample_trajectories(
+                env, actor, params['eval_batch_size'], params['ep_len']
+            )
+            logs = utils.compute_metrics(trajs, eval_trajs)
+
+            # compute additional metrics
+            logs.update(training_logs[-1])      # 최신의 training loss 추가
+            logs['Train_EnvstepsSoFar'] = total_envsteps
+            logs['TimeSinceStart'] = time.time() - start_time
+            if itr == 0: logs['Initial_DataCollection_AverageReturn'] = logs['Train_AverageReturn']
+
+            # perform the logging
+            for key, value in logs.items():
+                print('{}: {}'.format(key, value))
+                logger.log_scalar(value, key, iter)
+            print('Done logging. \n\n')
+
+            logger.flush()
+
+        if params['save_params']:
+            print('\n Saving agent params.')
+            filepath = str(Path(params['logdir']) / f'policy_itr_{itr}')
+            actor.save(filepath)
+
+
+if __name__ == "__main__":
+    params = parse_args()
+    run_training_loop(params)
